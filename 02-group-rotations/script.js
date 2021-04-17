981019860200e66fbcbf69c813015e2f6af85acd
Tommy Palmer <hi@tommyp.org>
2021-04-17 19:55:39 +0100

    turn into rectangles

--- 02-group-rotations/script.js
+++ 02-group-rotations/script.js
@@ -10,7 +10,7 @@ const params = {
 const two = new Two(params)
 two.appendTo(container)
 
-const numberOfShapes = 12
+const numberOfShapes = 40
 const plotRadius = 150
 
 const shapes = []
@@ -19,7 +19,7 @@ for (let i = 0; i < numberOfShapes; i++) {
   const angle = Math.PI * 2 * i / numberOfShapes
   const x = plotRadius * Math.cos(angle)
   const y = plotRadius * Math.sin(angle)
-  const shape = two.makeRectangle(x, y, 50, 50)
+  const shape = two.makeRectangle(x, y, 10, 200)
   shape.fill = "#f9bc31"
   shape.rotation = angle
   shape.noStroke()
@@ -32,9 +32,9 @@ group.translation.set(250, 250)
 
 two.bind('update', () => {
   shapes.forEach((shape) => {
-    shape.rotation += 0.025
+    shape.rotation += 0.004
   })
-  group.rotation += 0.005
+  group.rotation += 0.000615
 })
 
 two.play()
